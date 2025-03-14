cd320963f15a8762b632d7427e22e16483892276
solji <solji.choi89@gmail.com>
2025-03-14 10:23:02 +0900

    setting

---
+++ src/test/kotlin/com/ll/kp_2025_03_14/Kp20250314ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ll.kp_2025_03_14
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Kp20250314ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
