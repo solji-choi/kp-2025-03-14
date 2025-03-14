6a71125493a26773522b7f33bf67c49c20cd5fe5
solji <solji.choi89@gmail.com>
2025-03-14 12:34:02 +0900

    s3 service & test

--- src/main/kotlin/com/back/HomeController.kt
+++ src/main/kotlin/com/back/HomeController.kt
@@ -2,22 +2,18 @@ package com.back
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import software.amazon.awssdk.services.s3.S3Client
 
 @RestController
 class HomeController(
-    private val s3Client: S3Client
+    private val s3Service: S3Service
 ) {
     @GetMapping("/")
     fun main(): String {
-        return "HI!"
+        return "Hi"
     }
 
     @GetMapping("/buckets")
     fun buckets(): List<String> {
-        return s3Client
-            .listBuckets()
-            .buckets()
-            .map { it.name() }
+        return s3Service.getBucketNames()
     }
 }
